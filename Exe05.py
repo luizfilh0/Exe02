71f8fdb64a7c6d06e68a07abd1a55c05e452bd55
luizfilh0 <luizfilho.1998@hotmail.com>
2022-06-14 22:07:25 -0300

    atuali

---
+++ Exe05.py
@@ -0,0 +1,17 @@
+
+print('Olá, Bem vindo ao condificador de mensagens :)')
+print(' ')
+
+frase = str(input('Digite sua frase aqui: '))
+
+print(' ')
+print('Sua frase é: {}'.format(frase))
+print(' ')
+
+string = frase[::-1]
+
+print('PAM PAM PAM PAM...')
+print(' ')
+
+print('Sua frase condificada: {}'.format(string))
+print(' ')
